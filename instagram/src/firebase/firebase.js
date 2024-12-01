22c1b265ce91dccecac5ed7b1ae853200da82121
Stanislav588 <staskornienko01@gmail.com>
2024-12-01 16:45:15 +0100

    #

--- instagram/src/firebase/firebase.js
+++ instagram/src/firebase/firebase.js
@@ -4,16 +4,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyCsdBxUbor6DKVsNbbmxQAVx45LjdwewZI",
-  authDomain: "instagram-80e27.firebaseapp.com",
-  databaseURL: "https://instagram-80e27-default-rtdb.firebaseio.com",
-  projectId: "instagram-80e27",
-  storageBucket: "instagram-80e27.firebasestorage.app",
-  messagingSenderId: "598234589633",
-  appId: "1:598234589633:web:d3b8c1b6ae50505f9c6740",
-  measurementId: "G-Z34YEHK699",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
-
+console.log(firebaseConfig);
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const storage = getStorage(app);
